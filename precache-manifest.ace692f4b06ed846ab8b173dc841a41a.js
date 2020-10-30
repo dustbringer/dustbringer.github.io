1a5dbee4c3bfbe6fb46638dfb15ae4865929c715
victa-desktop <24532111+dustbringer@users.noreply.github.com>
2020-10-30 11:24:55 +1100

    Updates

---
+++ precache-manifest.ace692f4b06ed846ab8b173dc841a41a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe056ae693c98ec4f749069210eae7c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ea6eaffb867e5f2eb192",
+    "url": "/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "a3df1f22cba5b732c84c",
+    "url": "/static/js/2.9276adcc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9276adcc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea6eaffb867e5f2eb192",
+    "url": "/static/js/main.a2ea0c34.chunk.js"
+  },
+  {
+    "revision": "f86eee3664f26130c705",
+    "url": "/static/js/runtime-main.f0d0105f.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/main/precache-manifest.08adb4f481f9ebfc56c67d4242794928.js"
+  "/precache-manifest.ace692f4b06ed846ab8b173dc841a41a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/main/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
