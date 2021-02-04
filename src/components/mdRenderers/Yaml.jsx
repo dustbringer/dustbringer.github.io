6f354babc38ebb67a51d1fd0a81a80aafb22eea5
victa-desktop <24532111+dustbringer@users.noreply.github.com>
2021-02-04 16:20:38 +1100

    Edited date to be english

--- src/components/mdRenderers/Yaml.jsx
+++ src/components/mdRenderers/Yaml.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 import styled from "styled-components";
+import moment from "moment";
 
 import yaml from "js-yaml";
 import Typography from "@material-ui/core/Typography";
 
 // TODO style this up
 const Yaml = (props) => {
-  const obj = yaml.load(props.value);
-  const ret = `Written by ${obj.author} on ${obj.date}`;
+  const { date, author } = yaml.load(props.value);
+  const display = `Written by ${author} on ${
+    date && moment(date, "DD-MM-YYYY", true).isValid()
+      ? moment(date, "DD-MM-YYYY").format("Do MMMM YYYY")
+      : "Unknown Date"
+  }`;
   return (
     <Typography gutterBottom variant="body2">
-      {ret}
+      {display}
     </Typography>
   );
 };
