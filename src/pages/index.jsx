a3e36cc9df780f9400b109e9be1268eee4ef6a40
victa-desktop <24532111+dustbringer@users.noreply.github.com>
2022-05-20 07:15:57 +0100

    Nicer landing page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,13 +2,23 @@ import React from "react";
 import { Helmet } from "react-helmet";
 import { navigate } from "gatsby";
 import { GlobalContext } from "../context/GlobalContext";
+import styled from "styled-components";
 
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 
 import Container from "../components/Container";
 import InternalLink from "../components/InternalLink";
-import { DivFlexCenterHInside } from "../components/styled/Divs";
+import { DivMarginCenterH } from "../components/styled/Divs";
+
+import ProfileBird from "../images/profile-bird-md.jpg";
+
+const CircleImage = styled.img`
+  border-radius: 50%;
+  width: 40%;
+  height: auto;
+  margin: 3vh auto;
+`;
 
 function HomePage() {
   return (
@@ -18,15 +28,36 @@ function HomePage() {
         <meta name="description" content="Home Page" />
       </Helmet>
       <Container maxWidth="md">
-        <Typography variant="h4">Home</Typography>
-        <Typography variant="body1" gutterBottom>
-          Hi, Welcome to my website! Here are some links to some of my stuff...
+        <CircleImage
+          src={ProfileBird}
+          alt="Profile Bird"
+          height="476"
+          width="476"
+        />
+        <Typography variant="h3" align="center">
+          Victor Zhang
         </Typography>
+        <Typography variant="body2" align="center" sx={{ marginBottom: "2em" }}>
+          Mathematics enthusiast and Software developer
+        </Typography>
+
+        <Typography variant="body1" align="center" gutterBottom>
+          Welcome to my website! Have a look around and you'll find a variety of
+          sentences, tools and pictures I've made. Head to{" "}
+          <InternalLink to="/about">About</InternalLink> and{" "}
+          <InternalLink to="/resume">Resume</InternalLink> to learn more about
+          me. You'll also find a handful of blog posts in{" "}
+          <InternalLink to="/posts">Posts</InternalLink>, and various
+          paraphernalia in <InternalLink to="/posts">Tools</InternalLink>. There
+          is so much to see so enjoy your stay!
+        </Typography>
+
+        <Typography variant="h5" align="center" sx={{ margin: "1em 0" }}>
+          Quick Links
+        </Typography>
+
         <Typography variant="body1" component="div" gutterBottom>
           <ul>
-            <li>
-              <InternalLink to="/posts">Posts</InternalLink>
-            </li>
             <li>
               Tools
               <ul>
